0b5eafd6ecdc2393a5b0917a4f18519711d1124e
NYB <niuyabeng@126.com>
2018-05-12 18:24:08 +0800

    按行解析

---
+++ C/Test/parse_line.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int main(int argc, char **argv)
+{
+    const char *header = "GET /index.html HTTP1.1\r\n"
+        "Accept-encoding: html\r\n"
+        ""
+
+}
