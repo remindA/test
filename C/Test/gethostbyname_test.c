6dc7fff7ef7417be9c43faa6ca38899ff660c6b4
NYB <niuyabeng@126.com>
2018-05-12 18:25:06 +0800

    含有空格

--- C/Test/gethostbyname_test.c
+++ C/Test/gethostbyname_test.c
@@ -6,19 +6,21 @@
 
 int main(int argc, char **argv)
 {
-    if(argc != 2)
+/*    if(argc != 2)
     {
         printf("Usage: %s hostname\n", argv[0]);
         return 0;
     }
+*/
     struct hostent *host;
-    if((host = gethostbyname(argv[1])) == NULL)
+    //if((host = gethostbyname(argv[1])) == NULL)
+    if((host = gethostbyname("192.168.1.1")) == NULL)
     {
         perror("gethostbyname");
         return 0;
     }
     char str[32] = {0};
-    printf("%s <--> %s\n", argv[1], inet_ntop(AF_INET, host->h_addr, str, sizeof(str)));
+    printf("%s <--> %s\n", "192.168.1.1", inet_ntop(AF_INET, host->h_addr, str, sizeof(str)));
     return 0;
 }
 
