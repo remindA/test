3f19a1232248390ab4905dec8bba7e91b3efb866
NYB <niuyabeng@126.com>
2018-06-27 12:35:14 +0800

    实现

--- Utils/utils_net/utils_net.h
+++ Utils/utils_net/utils_net.h
@@ -18,12 +18,19 @@
 #ifndef _UTILS_NET_H_
 #define _UTILS_NET_H_
 
-/* 创建套接字 */
-int create_socket(const char *ip, unsigned short port);
-int create_socket_nonblock(const char *ip, unsigned short port);
+/*
+ * 为防止全局变量的名称和局部变量相同
+ * 接口函数内定义的变量start with '_'
+ */
 
-/* 设置套接字 */
-int set_socket(int fd, int flag);
+int create_tcpsock(const char *ip, unsigned short port, int backlog);
+int create_tcpsock_nonblock(const char *ip, unsigned short port, int backlog);
+
+int create_udpsock(const char *ip, unsigned short port);
+int create_udpsock_nonblock(const char *ip, unsigned short port);
+
+int sock_set_nonblock(int _fd);
+int sock_set_reuseraddr(int _fd);
 
 /* 获取网卡信息 */
 int get_eth_ip(const char *ethname, char *ip);
