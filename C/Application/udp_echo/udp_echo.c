81a9b5ef0be8a4f8a827bfe42530c951bc0cf3f8
NYB <niuyabeng@126.com>
2018-08-24 09:37:54 +0800

    新增udp_echo应用

---
+++ C/Application/udp_echo/udp_echo.c
@@ -0,0 +1,69 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  udp_echo_server.c
+ *
+ *    Description:  udp回射服务器
+ *
+ *        Version:  1.0
+ *        Created:  2018年08月22日 17时15分08秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  NYB (), niuyabeng@126.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/select.h>
+#include "utils_net.h"
+
+int main(int argc, char **argv)
+{
+    if(argc != 2) {
+        printf("Usage: udp_echo port\n");
+        return 0;
+    }
+    int fd = create_udpsock("0.0.0.0", (unsigned short)atoi(argv[1]));
+    if(fd < 0) {
+        printf("Can not create udp sock on 0.0.0.0:%s\n", argv[1]);
+        return 0;
+    }
+    sock_set_nonblock(fd);
+
+    struct sockaddr_in peer;
+    socklen_t peer_len = sizeof(peer_len);
+    fd_set rset;
+    FD_ZERO(&rset);
+    int ret;
+    while(1) {
+        FD_SET(fd, &rset);
+        ret = select(fd+1, &rset, NULL, NULL, NULL);
+        if(ret < 0) {
+            perror("select()");
+            continue;
+        }
+        else if(ret == 0) {
+            printf("select timeout\n");
+            continue;
+        }
+        else {
+            char buff[2048] = {0};
+            ret = recvfrom(fd, buff, sizeof(buff)-1, 0, (struct sockaddr *)&peer, &peer_len);
+            if(ret < 0) {
+                perror("recvfrom()");
+            }
+            else {
+                sendto(fd, buff, ret, 0, (struct sockaddr *)&peer, peer_len);
+            }
+        }
+    }
+    return 0;
+}
+
---
+++ C/Application/udp_echo/udp_test.c
@@ -0,0 +1,64 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  udp_echo_server.c
+ *
+ *    Description:  udp回射服务器测试客户端
+ *
+ *        Version:  1.0
+ *        Created:  2018年08月22日 17时15分08秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  NYB (), niuyabeng@126.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "utils_net.h"
+
+int main(int argc, char **argv)
+{
+    if(argc != 3) {
+        printf("Usage: udp_echo ip port\n");
+        return 0;
+    }
+
+    unsigned short port;
+    int fd = create_udpsock_rand_port("0.0.0.0", &port);
+    if(fd < 0) {
+        printf("Can not create udp sock\n");
+        return 0;
+    }
+    struct sockaddr_in peer;
+    memset(&peer, 0, sizeof(peer));
+    peer.sin_family = AF_INET;
+    peer.sin_port = htons(atoi(argv[2]));
+    peer.sin_addr.s_addr = inet_addr(argv[1]);
+    socklen_t peer_len = sizeof(peer);
+
+    fd_set rset;
+    FD_ZERO(&rset);
+    int ret;
+    char *content = "This is a udp test program.\n";
+    while(1) {
+        char buff[2048] = {0};
+        sendto(fd, content, strlen(content), 0, (struct sockaddr *)&peer, peer_len);
+        ret = recvfrom(fd, buff, sizeof(buff)-1, 0, (struct sockaddr *)&peer, &peer_len);
+        if(ret < 0) {
+            perror("recvfrom()");
+        }
+        else {
+            printf("%s", buff);
+        }
+        sleep(1);
+    }
+    return 0;
+}
+
